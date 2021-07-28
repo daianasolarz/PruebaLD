de16798a8e79afd6cdb3642dfa324ff4062b183e
Daiana Solarz <daianasolarz@gmail.com>
2021-07-28 20:25:09 -0300

    Agregue main js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let bienvenida = "Bienvenida/o a"
+let lei = "Lei Doiny Cakes"
+
+
+console.log("Hola" + bienvenida + lei);
+
+let guardar= prompt( "Ingrese su nombre y apellido")
+    document.write(guardar)
+    console.log(guardar)
+    alert("Hola"+ " "+ guardar + "!"+ " "+ bienvenida + " " + lei)
+    
+let edad = parseInt(prompt("Que edad tenes?"))
+document.write(guardar)
+console.log(guardar)
+
+
+
