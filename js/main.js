918a3ea9d5b305f126e2366d800498fe7c6b5929
Daiana Solarz <daianasolarz@gmail.com>
2021-08-02 18:45:23 -0300

    Agregué condicionales

--- js/main.js
+++ js/main.js
@@ -4,14 +4,21 @@ let lei = "Lei Doiny Cakes"
 
 console.log("Hola" + bienvenida + lei);
 
-let guardar= prompt( "Ingrese su nombre y apellido")
-    document.write(guardar)
-    console.log(guardar)
-    alert("Hola"+ " "+ guardar + "!"+ " "+ bienvenida + " " + lei)
+let guardar= prompt( "Ingrese su nombre y apellido");
+    if(guardar == ""){
+    alert("No ingresaste tu nombre")}
+    else{ alert("Hola"+ " "+ guardar + "!"+ " "+ bienvenida + " " + lei)} 
     
-let edad = parseInt(prompt("Que edad tenes?"))
-document.write(guardar)
+/* let edad = parseInt(prompt("Que edad tenes?"))
 console.log(guardar)
+console.log(guardar) */
 
+let año= prompt("Adivina cuando empezamos a cocinar!")
 
+if(año == 2018){
+    alert ("Ganaste!:)")
+} 
+else {
+    alert ("Intentalo nuevamente! :(")
+}
 
