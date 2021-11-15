6723b07ee49d59c5888b0944991ccfed423c6242
Daiana Solarz <daianasolarz@gmail.com>
2021-11-14 21:20:56 -0300

    Agregando Eventos pero recibo respuestas mal

--- js/principal.js
+++ js/principal.js
@@ -3,7 +3,7 @@ alert (`Hola ${bienvenida}! Bienvenido a Lei Doiny Cakes!` )
 
 let inicio = document.createElement ("div");
 inicio.innerHTML = "<div> <p>Inicia Sesión</p> </div>" ;
-document.getElementById("menu").appendChild (inicio);
+document.getElementById("menu").appendChild(inicio);
 
 inicioSesion = () => {
     let mail = prompt("Ingrese su mail")
@@ -20,3 +20,16 @@ alert (`Tu carrito tiene ${localStorage.getItem("carrito")}`)
 }
 carro.addEventListener ("click",respuestaClick)
 
+let nombre = document.getElementsByClassName("nombre").value;
+let celular = document.getElementsByClassName("celular").value;
+let mail = document.getElementsByClassName("mail").value;
+
+
+miFormulario = document.getElementById("Bueno");
+miFormulario.addEventListener("click", validarFormulario);
+
+function validarFormulario(e){
+    e.preventDefault();
+    console.log(`Los datos ingresados son ${nombre} y ${mail}`);}
+
+ /* LOS EVENTOS FUNCIONAN PERO NO ME DAN LA INFO QUE NECESITO :( */
