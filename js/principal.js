a7b5ebaa4b3fca0ae7f129cf4bc33e577a3dee43
Daiana Solarz <daianasolarz@gmail.com>
2021-11-16 22:21:01 -0300

    Agregando JQuery

--- js/principal.js
+++ js/principal.js
@@ -1,35 +1,37 @@
-let bienvenida = prompt ("Cómo es tu nombre?")
+$(() => 
+{ let bienvenida = prompt ("Cómo es tu nombre?")
 alert (`Hola ${bienvenida}! Bienvenido a Lei Doiny Cakes!` )
 
 let inicio = document.createElement ("div");
-inicio.innerHTML = "<div> <p>Inicia Sesión</p> </div>" ;
-document.getElementById("menu").appendChild(inicio);
+inicio.innerHTML = "<div id='prueba'> <p>Inicia Sesión</p> </div>" ;
+$("#menu").append(inicio);
 
-inicioSesion = () => {
+function inicioSesion () {
     let mail = prompt("Ingrese su mail")
     alert (`Ha ingresado sesión con ${mail}`)
 }
 
-inicio.addEventListener ("click",inicioSesion)
+$("#prueba").on("click",inicioSesion)
 
-    
-
-let carro = document.getElementById("Carrito")
+let carro = $("#Carrito")
 respuestaClick = () => { 
 alert (`Tu carrito tiene ${localStorage.getItem("carrito")}`)
 }
-carro.addEventListener ("click",respuestaClick)
-
-let nombre = document.getElementsByClassName("nombre").value;
-let celular = document.getElementsByClassName("celular").value;
-let mail = document.getElementsByClassName("mail").value;
+carro.on("click",respuestaClick);
 
 
-miFormulario = document.getElementById("Bueno");
-miFormulario.addEventListener("click", validarFormulario);
+let nombre = $(".nombre").value;
+let mail = $("mail").value;
+
+
+let miFormulario = $("#Bueno");
+miFormulario.on("click", validarFormulario);
 
 function validarFormulario(e){
     e.preventDefault();
     console.log(`Los datos ingresados son ${nombre} y ${mail}`);}
 
- /* LOS EVENTOS FUNCIONAN PERO NO ME DAN LA INFO QUE NECESITO :( */
+})
+
+
+ 
---
+++ js/recetas.js
@@ -0,0 +1,11 @@
+
+  $(".Aqui").append('<div class="toggle"><button id="btn1">No ver más</button></div)');
+
+  $(".toggle").on("click", function(){
+        $("section").toggle(2000)});
+        {
+            $("section").animate({
+                "opacity":2,
+                 "display":none})}
+  
+         
