f920c03ea0ae2d6fd319a7a2ae8c9ff058306cdc
Daiana Solarz <daianasolarz@gmail.com>
2021-11-22 20:45:50 -0300

    Agregando Ajax

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const Productos = [{id:1, nombre: "Chocotorta", precio:"$2600", tipo: "torta", img: "./imagenes/Chotot.jpg"},
-                {id:2, nombre: "ChocOreo", precio:"$2750", tipo: "torta", img: "./imagenes/Oreo.jpg"},
-                {id:3, nombre: "Maracuya", precio:"$2800", tipo: "torta", img: "./imagenes/Maracuya.jpg"},
-                {id:4, nombre: "Frutos Rojos", precio:"$2800", tipo: "torta", img: "./imagenes/Frutos rojos.jpg"},
-                {id:5, nombre: "Lemonpie", precio:"$2500", tipo: "torta", img: "./imagenes/DSC_8720.jpg"},
+const Productos = [{id:1, nombre: "Chocotorta", precio:"2600", tipo: "torta", img: "./imagenes/Chotot.jpg"},
+                {id:2, nombre: "ChocOreo", precio:"2750", tipo: "torta", img: "./imagenes/Oreo.jpg"},
+                {id:3, nombre: "Maracuya", precio:"2800", tipo: "torta", img: "./imagenes/Maracuya.jpg"},
+                {id:4, nombre: "Frutos Rojos", precio:"800", tipo: "torta", img: "./imagenes/Frutos rojos.jpg"},
+                {id:5, nombre: "Lemonpie", precio:"2500", tipo: "torta", img: "./imagenes/DSC_8720.jpg"},
                 {id:6, nombre: "Mousse de Chocolate", precio:"2600", tipo: "torta", img: "./imagenes/DSC_8362.jpg"},
-                {id:7, nombre: "Desayuno 1", precio:"$3000", tipo: "especial", img: "./imagenes/Desayuno 1.jpeg"},
-                {id:8, nombre: "Huevos de Pascua", precio:"$2400", tipo: "especial", img: "./imagenes/Huevos.jpeg"},
-                {id:9, nombre: "Vasitos", precio:"$850", tipo: "especial", img: "./imagenes/Vasos.jpeg"},
-                {id:10, nombre: "Degustación", precio:"$4500", tipo: "especial", img: "./imagenes/cakes.jpeg"},
-                {id:11, nombre: "Brownie", precio:"$2300", tipo: "especial", img: "./imagenes/brownie.jpg"},
-                {id:12, nombre: "Día Especial", precio:"$2800", tipo: "torta", img: "./imagenes/padre.jpeg"}]
+                {id:7, nombre: "Desayuno 1", precio:"3000", tipo: "especial", img: "./imagenes/Desayuno 1.jpeg"},
+                {id:8, nombre: "Huevos de Pascua", precio:"2400", tipo: "especial", img: "./imagenes/Huevos.jpeg"},
+                {id:9, nombre: "Vasitos", precio:"850", tipo: "especial", img: "./imagenes/Vasos.jpeg"},
+                {id:10, nombre: "Degustación", precio:"4500", tipo: "especial", img: "./imagenes/cakes.jpeg"},
+                {id:11, nombre: "Brownie", precio:"2300", tipo: "especial", img: "./imagenes/brownie.jpg"},
+                {id:12, nombre: "Día Especial", precio:"2800", tipo: "torta", img: "./imagenes/padre.jpeg"}]
 
 
 let containerProductos = document.querySelector("#containerProductos")
@@ -21,11 +21,11 @@ function mostrarProd (array) {
     containerProductos.innerHTML = "";
     array.forEach(e => {
         containerProductos.innerHTML += `
-        <div class="card m-3 align-items-center" style="width: 12rem;">
+        <div class="card m-3 align-items-center justify-content-center" style="width: 12rem;">
         <img src="${e.img}" class="card-img-top" alt="...">
         <div class="card-body align-items-center">
           <h5 class="card-title m-3">${e.nombre}</h5>
-          <p>${e.precio}</p>
+          <p>$${e.precio}</p>
           <button type="button" class="btn btn-dark" id="comprare" onclick="capturar (${e.id})"> Añadir al Carrito</button>
           </div>
       </div>`
@@ -41,7 +41,7 @@ function mostrarCarrito(array){
             <tr>
                 <th scope="row" id="num">${i++}</th>
                 <td>${e.nombre}</td>
-                <td>${e.precio}</td>
+                <td>$${e.precio}</td>
                 <td><button class="btn btn-danger" onclick="quitar(${e.id})">X</button></td>
             </tr>` ;}  
 
@@ -99,7 +99,7 @@ function mostrarCarrito(array){
 $(()=>{
 
    const items = () =>{
-        $("#Carrito").text(`(${(Number($("#num")))})`);     
+        $("#Carrito").html(`(${(Number($("#num")))})`);     
  
       }     //por que no funciona?
       items ();
